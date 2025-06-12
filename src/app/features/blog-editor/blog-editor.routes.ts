b88df179b618a2e8a8a171b0323e63929956103f
abdullahfirdowsi <abdullahtrickings@gmail.com>
2025-06-12 12:46:06 +0530

    feat: update blog editor endpoint as /write

--- src/app/features/blog-editor/blog-editor.routes.ts
+++ src/app/features/blog-editor/blog-editor.routes.ts
@@ -4,11 +4,6 @@ import { authGuard } from '../../core/guards/auth.guard';
 export const blogEditorRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'write',
-    pathMatch: 'full'
-  },
-  {
-    path: 'write',
     loadComponent: () => import('./pages/blog-writer/blog-writer.component').then(c => c.BlogWriterComponent),
     canActivate: [authGuard]
   }
