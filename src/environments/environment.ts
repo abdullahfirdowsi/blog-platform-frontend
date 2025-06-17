5064afe2dac2196c5246afd4ee6ea6e9785ccd72
abdullahfirdowsi <abdullahtrickings@gmail.com>
2025-06-17 13:24:46 +0530

    update: environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,
-  // apiUrl: 'https://blogplatformbackend-o56z.onrender.com/api/v1',
+  production: false,
   apiUrl: 'http://127.0.0.1:8000/api/v1',
 };
